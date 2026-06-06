1bc37ac57e4a5b0d09f505c9f5641a1b65b4b070
DnBright <dnbright000@gmail.com>
2026-06-06 10:11:51 +0700

    “update”

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,19 @@ Route::get('/', function () {
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
 });
+
+Route::get('/admin/projects', function () {
+    return view('admin.projects');
+});
+
+Route::get('/admin/clients', function () {
+    return view('admin.clients');
+});
+
+Route::get('/admin/messages', function () {
+    return view('admin.messages');
+});
+
+Route::get('/admin/settings', function () {
+    return view('admin.settings');
+});
